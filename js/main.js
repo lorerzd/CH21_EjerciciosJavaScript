26a5b02fcdfc5ec5866a472cfc2e5f3a6c5f8fcf
lorena <lore.rdz2802@gmail.com>
2022-12-09 18:06:30 -0600

    Ejercicio 1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+//Ejercicio 1 Escribir una función que tome un arreglo de números y devuelva el número mayor de la lista.
+
+Math.max(5,8,4,9,30,5,7,45,32,34,24,14,19)
+console.log(Math.max(5,8,4,9,30,5,7,45,32,34,24,14,19));
+
+
