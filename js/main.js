190654be3c74c0e02be7b4368208935f794d89c9
lorena <lore.rdz2802@gmail.com>
2022-12-09 18:10:47 -0600

    Ejercicio 2

--- js/main.js
+++ js/main.js
@@ -6,3 +6,8 @@ Math.max(5,8,4,9,30,5,7,45,32,34,24,14,19)
 console.log(Math.max(5,8,4,9,30,5,7,45,32,34,24,14,19));
 
 
+//Ejercicio 2 María llega a su entrevista de trabajo para el rol de Desarrollador Junior y se le pide que resuelva este problema: 
+//Dada una lista de números [-1, 3, 4, 2, 6], escribe una función en javascript para calcular el número más pequeño.
+
+Math.min(-1, 3, 4, 2, 6)
+console.log(Math.min(-1, 3, 4, 2, 6));
