b37936d6b93fd49b878f396e5c1f67ac7caaf43c
lorena <lore.rdz2802@gmail.com>
2022-12-11 01:08:38 -0600

    Ejercicios parte 2 (1,2,4,5,6)

--- js/main.js
+++ js/main.js
@@ -79,3 +79,149 @@ var foo = [1, 2, 5, 14, 24, 31, 50, 105 ];
  
 console.log( foo.shuffle() ); 
 
+/////////////// PARTE 2 //////////////////////////////////////////////////// 
+
+//Ejercicio 1 Solicitar 3 números (entre el 1 y el 100) y definir cual es el mayor
+//Ejercicio 2 Solicitar 3 números (entre el 1 y el 100) y definir cual es el menor
+
+function numMayor(array){
+    let temp = Number.MIN_VALUE;
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] > temp){
+            temp = array[i];
+        }
+    }
+    return temp;
+}
+
+function numMenor(array){
+    let temp = Number.MAX_VALUE;
+    for (let index = 0; index < array.length; index++) {
+        if(array[index] < temp){
+            temp = array[index];
+        }
+    }
+    return temp;
+}
+
+let btnResultado = document.getElementById("btnResultado");
+
+btnResultado.addEventListener("click", function(e) {
+    e.preventDefault();
+    let inputOne = parseInt(document.getElementById("inputOne").value);
+    let inputTwo = parseInt(document.getElementById("inputTwo").value);
+    let inputThree = parseInt(document.getElementById("inputThree").value);
+    let array = [inputOne,inputTwo,inputThree];
+    let resultHigh = document.getElementById("resultHigh");
+    resultHigh.innerHTML = "El mayor es : "+numMayor(array);
+    let inputOneField = document.getElementById("inputOne");
+    inputOneField.focus();
+    inputOneField.select();
+  });
+
+  //Ejercicio 2 Solicitar 3 números (entre el 1 y el 100) y definir cual es el menor
+
+  let btnResultado2 = document.getElementById("btnResultado2");
+
+  btnResultado2.addEventListener("click", function(e) {
+      e.preventDefault();
+      let inputOne = parseInt(document.getElementById("inputUno").value);
+      let inputTwo = parseInt(document.getElementById("inputDos").value);
+      let inputThree = parseInt(document.getElementById("inputTres").value);
+      let array = [inputOne,inputTwo,inputThree];
+      let resultLow = document.getElementById("resultLow");
+      resultLow.innerHTML = "El menor es : "+numMenor(array)+"<br>";
+      let inputOneField = document.getElementById("inputUno");
+      inputOneField.focus();
+      inputOneField.select();
+    });
+
+
+
+
+
+
+  //Ejercicio 4 Solicitar un entero (entre el 100 y el 200) y determinar si es múltiplo de 3
+
+  let multiplo = document.getElementById("multiplo");
+
+  multiplo.addEventListener("click", function(e) {
+    e.preventDefault();
+    let inputMultiplo = parseInt(document.getElementById("inputMultiplo").value);
+    let multiploResult = document.getElementById("multiploResult");
+    if(inputMultiplo >= 100 && inputMultiplo <= 200){
+        multiploResult.innerHTML = multipleOfThree(inputMultiplo);
+    }else{
+        multiploResult.innerHTML = "No es un numero entre 100 y 200"
+    }
+  });
+
+  function multipleOfThree(number){
+    if(number % 3 == 0){
+        return "Es multiplo";
+    }else{
+        return "No es multiplo";
+    }
+}
+
+//Ejercicio 5 Elaborar un algoritmo para leer 3 números y determinar sí uno es la suma de los otros dos
+
+let isSum = document.getElementById("isSum");
+
+isSum.addEventListener("click", function(e) {
+    e.preventDefault();
+    let inputOne = parseInt(document.getElementById("firstInput").value);
+    let inputTwo = parseInt(document.getElementById("secondInput").value);
+    let inputThree = parseInt(document.getElementById("thirdInput").value);
+    let array = [inputOne,inputTwo,inputThree];
+    let resultIsSum = document.getElementById("resultIsSum");
+    if(isSumFn(array) != -1){
+        resultIsSum.innerHTML = "El número es : "+isSumFn(array);
+    }else{
+        resultIsSum.innerHTML = "No es suma";
+    }
+    let inputOneField = document.getElementById("inputOne");
+    inputOneField.focus();
+    inputOneField.select();
+  });
+
+function isSumFn(array){
+    for (let i = 0; i < array.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < array.length; j++) {
+            if(i == j) continue;
+            sum += array[j];
+        }
+        if(sum == array[i]){
+            return array[i];
+        }
+    }
+    return -1;
+
+}
+
+//Ejercicio 6 Elabora un algoritmo para leer un número y determinar si es par o impar
+
+let parImpar = document.getElementById("parImpar");
+
+  parImpar.addEventListener("click", function(e) {
+    e.preventDefault();
+    let inputPar = parseInt(document.getElementById("inputPar").value);
+    let resultPar = document.getElementById("resultParImpar");
+    if(inputPar % 2 == 0){
+        resultPar.innerHTML = "Es par";
+    }else{
+        resultPar.innerHTML = "Es impar"
+    }
+  });
+
+
+
+
+
+
+
+
+//Ejercicio 2 Solicitar 3 números (entre el 1 y el 100) y definir cual es el menor
+
+   
